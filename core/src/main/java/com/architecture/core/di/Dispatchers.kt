dfa4dc7f826532f02c216057703ddc8a64539221
Borja <borja@i2b.studio>
2024-03-11 16:15:20 +0100

    Create Dispatchers Hilt module to inject dispatchers

---
+++ core/src/main/java/com/architecture/core/di/Dispatchers.kt
@@ -0,0 +1,7 @@
+package com.architecture.core.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IODispatcher
---
+++ core/src/main/java/com/architecture/core/di/DispatchersModule.kt
@@ -0,0 +1,17 @@
+package com.architecture.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatchersModule {
+
+    @IODispatcher
+    @Provides
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
